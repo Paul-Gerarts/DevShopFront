f9a8a63ad11e0f871e22ee4afcd8060cb93ebb7e
lens <lens.huygh@gmail.com>
2020-03-03 21:23:35 +0100

    add application.properties with port, thymeleaf settings add controller package add MainPageController class add RequestMapping to display index.html add index.html with title to templates folder

---
+++ src/main/java/be/syntra/devshop/DevshopFront/controller/MainPageController.java
@@ -0,0 +1,16 @@
+package be.syntra.devshop.DevshopFront.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/devshop")
+public class MainPageController {
+
+    @GetMapping
+    public ModelAndView displayMainPage(){
+        return new ModelAndView("index");
+    }
+}
