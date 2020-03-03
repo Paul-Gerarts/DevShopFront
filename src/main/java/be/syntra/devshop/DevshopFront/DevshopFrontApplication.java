8300e765c4b78c8fa8dfe0365dda6526a6dd8de6
lens <lens.huygh@gmail.com>
2020-03-03 19:50:24 +0100

    create project with -Spring Security                     -Spring Web                     -JPA                     -Lombok                     -DevTools add Git version control setup .gitignore file with a Team DevShop section and add maven non-needed files

---
+++ src/main/java/be/syntra/devshop/DevshopFront/DevshopFrontApplication.java
@@ -0,0 +1,13 @@
+package be.syntra.devshop.DevshopFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevshopFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevshopFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/syntra/devshop/DevshopFront/DevshopFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package be.syntra.devshop.DevshopFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevshopFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
