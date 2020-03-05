d14e844ef71108d7ac11ee6dbd3b3b401a017dd7
lens <lens.huygh@gmail.com>
2020-03-05 20:32:44 +0100

    finish externalizing css in application.properties the static folder needs be made accessible add ref to css to index.html to center dummy content

---
+++ src/main/java/be/syntra/devshop/DevshopFront/configuration/WebConfig.java
@@ -0,0 +1,19 @@
+package be.syntra.devshop.DevshopFront.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/css/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/static/css/",
+                        "classpath:/static/js/"
+                );
+    }
+}
