f80820ff219d8d435f03756be62a1cdff80d7aa7
lens <lens.huygh@gmail.com>
2020-03-06 19:37:34 +0100

    add url and endpoint to application.properties add reading url and endpoint strings from properties to ProductService

--- src/main/java/be/syntra/devshop/DevshopFront/service/ProductServiceImpl.java
+++ src/main/java/be/syntra/devshop/DevshopFront/service/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package be.syntra.devshop.DevshopFront.service;
 
 import be.syntra.devshop.DevshopFront.model.SaveStatus;
 import be.syntra.devshop.DevshopFront.model.dto.ProductDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -10,6 +11,10 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+    @Value("${baseUrl}")
+    String baseUrl;
+    @Value("${productsEndpoint}")
+    String endpoint;
 
     @Override
     public ProductDto createEmptyProduct() {
@@ -18,12 +23,13 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public SaveStatus addProduct(ProductDto productDto) {
+        final String url = baseUrl.concat(endpoint);
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<ProductDto> request = new HttpEntity<>(productDto, httpHeaders);
         try {
-            ProductDto productDtoResultFromBackEnd = restTemplate.postForObject("http://localhost:8080/products", request, ProductDto.class);
+            ProductDto productDtoResultFromBackEnd = restTemplate.postForObject(url, request, ProductDto.class);
             if (productDto.equals(productDtoResultFromBackEnd)) {
                 return SaveStatus.SAVED;
             }
