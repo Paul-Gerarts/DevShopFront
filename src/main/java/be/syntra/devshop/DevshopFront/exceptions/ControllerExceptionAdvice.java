15356e2ee91f5faffa1a828524341d68de052c51
Fonthom <thomasf0n7a1n3@gmail.com>
2020-04-01 19:27:22 +0200

    DEV-018 made ControllerExceptionAdvice

---
+++ src/main/java/be/syntra/devshop/DevshopFront/exceptions/ControllerExceptionAdvice.java
@@ -0,0 +1,30 @@
+package be.syntra.devshop.DevshopFront.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ControllerExceptionAdvice extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiError> handleCustomException(CustomException exception) {
+        if (exception.getHttpStatus().is5xxServerError()) {
+            logger.error(exception, exception);
+        } else {
+            logger.warn(exception, exception);
+        }
+        return new ResponseEntity<>(
+                ApiError.builder()
+                        .status(exception.getHttpStatus().getReasonPhrase())
+                        .code(exception.getHttpStatus().value())
+                        .build(),
+                exception.getHttpStatus()
+        );
+        @ExceptionHandler
+        public ResponseEntity<String> handleException (Exception exception){
+            return new ResponseEntity<?>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
---
+++ src/main/java/be/syntra/devshop/DevshopFront/models/ApiError.java
@@ -0,0 +1,4 @@
+package be.syntra.devshop.DevshopFront.models;
+
+public class ApiError {
+}
