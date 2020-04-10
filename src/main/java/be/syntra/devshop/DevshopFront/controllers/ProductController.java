813a7dca30782d24a622cb16174ecc77ffe88b70
lens <lens.huygh@gmail.com>
2020-04-10 18:20:01 +0200

    add addtoCart method to ProductController to handle adding product to cart

--- src/main/java/be/syntra/devshop/DevshopFront/controllers/ProductController.java
+++ src/main/java/be/syntra/devshop/DevshopFront/controllers/ProductController.java
@@ -3,16 +3,15 @@ package be.syntra.devshop.DevshopFront.controllers;
 import be.syntra.devshop.DevshopFront.models.Product;
 import be.syntra.devshop.DevshopFront.models.StatusNotification;
 import be.syntra.devshop.DevshopFront.services.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/products")
 public class ProductController {
@@ -45,5 +44,11 @@ public class ProductController {
         model.addAttribute("product", product);
         model.addAttribute("status", statusNotification);
         return "product/productDetails";
+    }
+
+    @PostMapping
+    public String addSelectedProductToCart(@ModelAttribute("id") Integer id) {
+        log.info("chosen product -> " + id);
+        return "redirect:/products";
     }
 }
