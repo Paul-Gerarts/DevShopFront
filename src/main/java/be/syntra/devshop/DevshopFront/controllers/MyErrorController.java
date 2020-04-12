c26abe8f206ba2e14b0d0958506bdc5f3c9ba34f
Paul Gerarts <paul.gerarts@juvo.be>
2020-04-12 21:06:07 +0200

    DEV-028 customizing whitelabel error
    Added all logic needed for custom error page

---
+++ src/main/java/be/syntra/devshop/DevshopFront/controllers/MyErrorController.java
@@ -0,0 +1,48 @@
+package be.syntra.devshop.DevshopFront.controllers;
+
+import be.syntra.devshop.DevshopFront.models.dto.ErrorDto;
+import be.syntra.devshop.DevshopFront.services.ErrorService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Controller
+public class MyErrorController implements ErrorController {
+
+    private final ErrorService errorService;
+
+    @Autowired
+    public MyErrorController(ErrorService errorService) {
+        this.errorService = errorService;
+    }
+
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        return displayCustomErrorPage(request, model);
+    }
+
+    /*
+     * Getting rid of that annoying GET-error for favicon.ico
+     */
+    @GetMapping("/favicon.ico")
+    public String handleServletError(HttpServletRequest request, Model model) {
+        return displayCustomErrorPage(request, model);
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    private String displayCustomErrorPage(HttpServletRequest request, Model model) {
+        ErrorDto thrownError = errorService.determineError(request);
+        model.addAttribute("thrownError", thrownError);
+        return "fragments/whitelabelError";
+    }
+}
---
+++ src/main/java/be/syntra/devshop/DevshopFront/services/ErrorServiceImpl.java
@@ -0,0 +1,38 @@
+package be.syntra.devshop.DevshopFront.services;
+
+import be.syntra.devshop.DevshopFront.models.dto.ErrorDto;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Service
+public class ErrorServiceImpl implements ErrorService {
+
+    @Override
+    public ErrorDto determineError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String errorCode = "nullpointer on status";
+        String message = errorCode;
+
+        if (null != status) {
+            switch (status.toString()) {
+                case "400":
+                    errorCode = status.toString() + " BAD REQUEST";
+                    message = "We had trouble fetching what you wanted...";
+                    break;
+                case "404":
+                    errorCode = status.toString() + " NOT FOUND";
+                    message = "We couldn't find what you're searching for...";
+                    break;
+                default:
+                    errorCode = "";
+                    message = "Something went wrong...";
+            }
+        }
+        return ErrorDto.builder()
+                .errorCode(errorCode)
+                .message(message)
+                .build();
+    }
+}
