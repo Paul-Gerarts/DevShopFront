11ab184138783948160f0edc87f03f980f6f9faa
Fonthom <thomasf0n7a1n3@gmail.com>
2020-04-13 22:24:25 +0200

    DEV-030  optimization exceptionHandling

--- src/main/java/be/syntra/devshop/DevshopFront/exceptions/GlobalFormExceptionHandler.java
+++ src/main/java/be/syntra/devshop/DevshopFront/exceptions/GlobalFormExceptionHandler.java
@@ -1,8 +1,6 @@
 package be.syntra.devshop.DevshopFront.exceptions;
 
 
-import be.syntra.devshop.DevshopFront.models.dto.ProductDto;
-import be.syntra.devshop.DevshopFront.models.dto.RegisterUserDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,25 +13,9 @@ import java.util.Objects;
 public class GlobalFormExceptionHandler {
 
     @ExceptionHandler({MethodArgumentNotValidException.class})
-    public String validationError(MethodArgumentNotValidException ex) {
-        if (ex.getBindingResult().hasErrors()) {
-            for (String code : Objects.requireNonNull(Objects.requireNonNull(ex.getBindingResult().getFieldError()).getCodes())) {
-                log.error(code);
-            }
-            if (isProductDto(ex)) {
-                return "admin/product/addProduct";
-            } else if (isRegisterDto(ex)) {
-                return "/user/register";
-            }
+    public void validationError(MethodArgumentNotValidException ex) {
+        for (String code : Objects.requireNonNull(Objects.requireNonNull(ex.getBindingResult().getFieldError()).getCodes())) {
+            log.error(code);
         }
-        return "products";
-    }
-
-    private boolean isRegisterDto(MethodArgumentNotValidException ex) {
-        return ex.getParameter().getParameterType().equals(RegisterUserDto.class);
-    }
-
-    private boolean isProductDto(MethodArgumentNotValidException ex) {
-        return ex.getParameter().getParameterType().equals(ProductDto.class);
     }
 }
